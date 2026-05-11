e5cc1e31071a49487b4a12c86414aa699726585c
vivicky27 <vivivickyok@gmail.com>
2026-05-11 15:21:29 -0400

    Clean repository files

--- hls/sobel_hls/solution1/sim/verilog/sim.bat
+++
@@ -1,1 +0,0 @@
-    F:/Xilinx/Vitis_HLS/2024.1/bin/vitis_hls run_sim.tcl
--- hls/sobel_hls/solution1/sim/verilog/xsim.dir/sobel_accel/xsim_script.tcl
+++
@@ -1,1 +0,0 @@
-xsim {sobel_accel} -view {{sobel_accel_dataflow_ana.wcfg}} -tclbatch {sobel_accel.tcl} -protoinst {sobel_accel.protoinst}
